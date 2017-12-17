95cc0fb2a0ff97abd44f68e6bd42bb9bc1f0728e
Joakim Fristedt <joakim.fristedt@findwise.com>
2017-12-17 17:11:36 +0100

    Updates

--- install_git_extras.sh
+++ install_git_extras.sh
@@ -5,8 +5,8 @@ completion="git-completion.bash"
 prompt="git-prompt.sh"
 
 install() {
-    wget --quiet $base_url/$1
-    ln -fs $PWD/$1 ~/.$1
+    wget --quiet -nc -P $PWD/git $base_url/$1
+    ln -fs $PWD/git/$1 ~/.$1
 }
 
 install $prompt
--- link.sh
+++ link.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-ln -fs $PWD/tmux.conf ~/.tmux.conf
-ln -fs $PWD/inputrc ~/.inputrc
-ln -fs $PWD/bash_aliases ~/.bash_aliases
-ln -fs $PWD/bash_profile ~/.bash_profile
-ln -fs $PWD/bashrc ~/.bashrc
-ln -fs $PWD/vimrc ~/.vimrc
-ln -fs $PWD/ideavimrc ~/.ideavimrc
+ln -fs $PWD/bash/inputrc ~/.inputrc
+ln -fs $PWD/bash/bash_aliases ~/.bash_aliases
+ln -fs $PWD/bash/bash_profile ~/.bash_profile
+ln -fs $PWD/bash/bashrc ~/.bashrc
+
+ln -fs $PWD/tmux/tmux.conf ~/.tmux.conf
+
+ln -fs $PWD/vim/vimrc ~/.vimrc
+ln -fs $PWD/vim/ideavimrc ~/.ideavimrc
+
+ln -fs $PWD/git/gitignore ~/.gitignore

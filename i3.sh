83dfe8d158f62b30dcd84a1065457eb31e0db92d
Joakim Fristedt <joakim.fristedt@findwise.com>
2018-10-08 20:15:36 +0200

    adds script for display usage in i3

--- i3.sh
+++ i3.sh
@@ -6,3 +6,4 @@ mkdir -p ~/.i3/scripts
 ln -s $DIR/i3/config ~/.i3/config
 ln -s $DIR/i3/i3blocks.conf ~/.i3/i3blocks.conf
 ln -s $DIR/i3/scripts/volume ~/.i3/scripts/volume
+ln -s $DIR/i3/scripts/display ~/.i3/scripts/display

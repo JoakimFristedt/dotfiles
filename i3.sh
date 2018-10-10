2e7978283b61d1dd4671a94a63da72ba1813ec3b
Joakim Fristedt <joakim.fristedt@findwise.com>
2018-10-10 20:38:48 +0200

    Updates i3 config

--- i3.sh
+++ i3.sh
@@ -7,3 +7,4 @@ ln -s $DIR/i3/config ~/.i3/config
 ln -s $DIR/i3/i3blocks.conf ~/.i3/i3blocks.conf
 ln -s $DIR/i3/scripts/volume ~/.i3/scripts/volume
 ln -s $DIR/i3/scripts/display ~/.i3/scripts/display
+ln -s $DIR/i3/scripts/screenshot ~/.i3/scripts/screenshot

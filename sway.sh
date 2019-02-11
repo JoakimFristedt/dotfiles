34a130ff22ff380f42ba40fabd43ef47d254fa85
Joakim Fristedt <joakim.fristedt@findwise.com>
2019-02-11 16:32:40 +0100

    Removes sway

--- sway.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-mkdir -p ~/.config/sway/scripts
-ln -sf $DIR/i3/config ~/.config/sway/config
-ln -sf $DIR/i3/i3blocks.conf ~/.config/sway/i3blocks.conf
-ln -sf $DIR/i3/scripts/volume ~/.config/sway/scripts/volume
